c23fd90c262c5f72e4b75f5c774baf10991f028e
mamesalo <117835018+mamesalo@users.noreply.github.com>
2024-11-11 12:06:47 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Outlet,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Features from "./components/Features";
-import Enterprise from "./components/Enterprise";
-import Home from "./components/Home";
-import Support from "./components/Support";
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route
-          path="/"
-          element={
-            <div className="flex flex-col h-full">
-              <Header />
-              <div className="flex-1 flex items-center justify-center font-semibold text-4xl">
-                <Outlet />
-              </div>
-              <Footer />
-            </div>
-          }
-        >
-          <Route index path="home" element={<Home />} />
-          <Route path="enterprise" element={<Enterprise />} />
-          <Route path="features" element={<Features />} />
-          <Route path="support" element={<Support />} />
-        </Route>
-      </Route>
-    )
-  );
   return (
-    <div className="h-screen max-w-6xl mx-auto p-4 text-slate-900">
-      <RouterProvider router={router} />
+    <div className="h-screen max-w-6xl mx-auto p-4 text-slate-900 flex flex-col">
+      <Header />
+      <Features />
+      <Footer />
     </div>
   );
 };
