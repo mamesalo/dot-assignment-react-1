b8933f57435b9cb2c68bea1161b39b639a07fb48
mamesalo <117835018+mamesalo@users.noreply.github.com>
2024-11-11 12:10:10 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,20 +19,32 @@ const Header = () => {
       to: "home",
     },
   ];
+
   return (
-    <header className="py-2 px-10 flex justify-between border-b-2 ">
+    <header className="py-2 px-10 flex justify-between border-b-2 border-gray-500">
       <h1 className="font-semibold text-lg">Company name</h1>
       <nav>
         <ul className="flex gap-5">
-          {navLinks.map((item, index) => (
-            <Link
-              to={item.to}
-              key={index}
-              className="text-lg underline underline-offset-4"
-            >
-              {item.label}
-            </Link>
-          ))}
+          <li>
+            <a href="#" className="text-lg underline underline-offset-4">
+              Features
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg underline underline-offset-4">
+              Enterprise
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg underline underline-offset-4">
+              Support
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg underline underline-offset-4">
+              Home
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
