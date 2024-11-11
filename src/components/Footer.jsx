a8b73062f9522d7bddf514002913144139301f86
mamesalo <117835018+mamesalo@users.noreply.github.com>
2024-11-11 12:09:20 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,42 @@
 const Footer = () => {
   return (
-    <footer className="flex gap-4 text-center justify-evenly ">
+    <footer className="flex gap-4 text-center justify-evenly border-t-2 border-gray-500 pt-2">
       <div>
         <p className="font-semibold text-lg mb-2">Features</p>
         <ul className="underline">
-          <li>Cool Stuff</li>
-          <li>Random Feature</li>
-          <li>Team Feature</li>
+          <li>
+            <a href="#" className="text-blue-800">
+              Cool Stuff
+            </a>
+          </li>
+          <li>
+            <a href="#">Random Feature</a>
+          </li>
+          <li>
+            <a href="#">Team Feature</a>
+          </li>
         </ul>
       </div>
       <div>
         <p className="font-semibold text-lg mb-2">Resources</p>
         <ul className="underline">
-          <li>Resource</li>
-          <li>Resource Name</li>
+          <li>
+            <a href="#">Resource</a>
+          </li>
+          <li>
+            <a href="#">Resource Name</a>
+          </li>
         </ul>
       </div>
       <div>
         <p className="font-semibold text-lg mb-2">About</p>
         <ul className="underline">
-          <li>Team</li>
-          <li>Location</li>
+          <li>
+            <a href="#">Team</a>
+          </li>
+          <li>
+            <a href="#">Location</a>
+          </li>
         </ul>
       </div>
     </footer>
